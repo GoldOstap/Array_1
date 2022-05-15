664b43f539f006673681248e9ed25fa862255eda
GoldOstap <izzheurov@mail.ru>
2022-05-15 21:01:07 +0400

    1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int k =0;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Введи размер массива: ");
+        k = scan.nextInt();
+
+        int[] myArray= new int[--k];
+
+        for (int t=0; t<myArray.length; t++)
+            myArray[t]=(t+1)*2;
+
+        for (int t=0; t<myArray.length; t++)
+            System.out.print(myArray[t]+" ");
+        System.out.println();
+        
+        for (int t=0; t<myArray.length; t++)
+            System.out.println(myArray[t]);
+    }
+}
